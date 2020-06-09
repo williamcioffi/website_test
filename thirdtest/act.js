7f1110e50931614dc22191bf3fdf145abba51fbc
williamcioffi <william.cioffi@gmail.com>
2020-06-09 19:52:35 -0400

    one pager test

--- thirdtest/act.js
+++ thirdtest/act.js
@@ -4,7 +4,12 @@ const links = document.querySelectorAll(".navlinks li");
 
 burger.addEventListener("click", () => {
   navlinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+  
+  links.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navlinkfade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
 });
